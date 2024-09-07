3e32d18adb6ac39e1b52b71ab1a41bfe0984dde4
khom19 <khom2548@gmail.com>
2024-09-07 15:17:44 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 //We will copy youtube.
 function App() {
   return (
     <div className='Homepage'>
-      <header className='text'>
-        This is home page.
-      </header>
+      <div className='menu'>
+        <div className='menu_icon'>
+          <RxHamburgerMenu className='icon' size={22.5}/>
+          <div className='youtube_icon'></div>
+        </div>
+        <h1 className='youtube_name'>
+          Premium
+        </h1>
+      </div>
     </div>
   );
 }
