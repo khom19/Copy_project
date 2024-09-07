8222f7b8dce34289876bac8fa80c669e9c8f7fe4
khom19 <khom2548@gmail.com>
2024-09-07 16:07:26 +0700

    make youtube menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { RxHamburgerMenu } from "react-icons/rx";
+import { FaYoutube } from "react-icons/fa";
 
 //We will copy youtube.
 function App() {
@@ -8,10 +9,11 @@ function App() {
       <div className='menu'>
         <div className='menu_icon'>
           <RxHamburgerMenu className='icon' size={22.5}/>
-          <div className='youtube_icon'></div>
         </div>
         <h1 className='youtube_name'>
+          <FaYoutube className='youtube_icon' size={32.5}/>
           Premium
+          <p className='language'>TH</p>
         </h1>
       </div>
     </div>
